4bb6675ce74db8e745937c94abc45956e48716b1
Avinashpalofficial <apal73451@gmail.com>
2024-10-24 18:48:46 +0530

    update

--- ZEERODHA/Navbar/Layout.jsx
+++ ZEERODHA/Navbar/Layout.jsx
@@ -11,22 +11,50 @@ const Layout = () =>{
    else{
 
      return (
-         <> 
- 
-           <nav>
-             <table>
-              <th><Link to="/Logo">Logo</Link></th>
-              <th><Link to = "/Signup">Signup</Link></th>
-              <th><Link to= "/About">About</Link></th>
-              <th><Link to ="/Products">Products</Link></th>
-              <th><Link to = "/Pricing">Princing</Link></th>
-              <th><Link to ="/Support">Support</Link></th>
-             </table>
-               
-           </nav>      
-       <Outlet/>
-       </>
-     )
-   }
+            <div className="flex  h-24">
+              <div className="w-2/6 translate-x-[40%]">
+                
+                  
+                 <div className="w-40 inline-block my-6 ">
+                  <Link to= "/Logo"><img src="https://zerodha.com/static/images/logo.svg"  alt="" /></Link>
+                 </div>
+                
+              </div> 
+                 <div className=" max-w-6xl translate-x-[45%] ">
+                 <div className="flex justify-center mx-16 my-1   translate-x-10  space-x-1  ">
+                    <div className="w-28 h-20 pt-6 pl-6"><Link to= "/Signup">Signup</Link></div>
+                     <div className="w-28 h-20 pt-6 pl-6"><Link to= "/About">About</Link></div>
+                     <div className="w-40 h-20 pt-6 pl-10"><Link to ="/Products">Products</Link></div>
+                     <div className="w-28 h-20 pt-6 pl-6"><Link to = "/Pricing">Pricing</Link></div>
+                     <div className="w-28 h-20 pt-6 pl-6"><Link to ="Support">Support</Link></div>
+              
+                 </div>
+                 </div>
+            </div>
+
+    )
+    
+  }
+  
 }
 export default  Layout
+//        <> 
+//         <div className="flex justify-center  w-2xl">
+      
+//            <table>
+//             <div><th><Link to="/Logo">Logo</Link></th></div>
+//             <div className="flex justify-center">
+//             <th><Link to = "/Signup">Signup</Link></th>
+//             <th><Link to= "/About">About</Link></th>
+//             <th><Link to ="/Products">Products</Link></th>
+//             <th><Link to = "/Pricing">Princing</Link></th>
+//             <th><Link to ="/Support">Support</Link></th>
+//             </div>
+            
+//            </table>
+             
+              
+//         </div>
+         
+//      <Outlet/>
+//      </>
